abce7f52a63f1c4df56f059b74fd2203b06911cf
Vladislav Fedorov <contact@vladislavfedorov.ru>
2019-09-24 14:13:58 +0300

    first commit

---
+++ engine.js
@@ -0,0 +1,69 @@
+var image;
+
+function loadimg(){
+  var imgcanvas = document.getElementById("canvas-id");
+  var fileinput = document.getElementById("finput");
+  image = new SimpleImage(fileinput);
+  image.drawTo(imgcanvas);
+};
+
+function firstfilter(){
+  for (var pixel of image.values()){
+	var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+	pixel.setRed(avg);
+	pixel.setGreen(avg);
+	pixel.setBlue(avg);
+}
+var imgcanvas = document.getElementById("canvas-id");
+image.drawTo(imgcanvas);
+};
+
+function secondfilter(){
+  for (var pixel of image.values()){
+	var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+    if (avg < 250) {
+      pixel.setRed(avg*2);
+	    pixel.setGreen(avg-avg);
+	    pixel.setBlue(avg-avg);
+    } else if (avg <= 255 && avg > 250) {
+      pixel.setRed(255);
+	    pixel.setGreen(0);
+	    pixel.setBlue(0);
+    } else {
+      pixel.setRed((avg*2)-255);
+	    pixel.setGreen(255);
+	    pixel.setBlue((avg*2)-255);
+    }
+}
+var imgcanvas = document.getElementById("canvas-id");
+image.drawTo(imgcanvas);
+};
+
+function thirdfilter(){
+  for (var pixel of image.values()){
+	var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+      pixel.setRed(avg-avg);
+	    pixel.setGreen(avg*2);
+	    pixel.setBlue(avg-avg);
+}
+var imgcanvas = document.getElementById("canvas-id");
+image.drawTo(imgcanvas);
+};
+
+function fourthfilter(){
+  for (var pixel of image.values()){
+	var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+	pixel.setRed(avg-avg);
+	pixel.setGreen(avg-avg);
+	pixel.setBlue(avg*2);
+}
+var imgcanvas = document.getElementById("canvas-id");
+image.drawTo(imgcanvas);
+};
+
+function resetimg(){
+  var imgcanvas = document.getElementById("canvas-id");
+  var fileinput = document.getElementById("finput");
+  image = new SimpleImage(fileinput);
+  image.drawTo(imgcanvas);
+};
